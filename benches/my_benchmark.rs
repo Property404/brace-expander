9f4e78a033ce7bd06d7bcf93c2ce6e67310c50fb
Property404 <git@dagans.dev>
2026-05-30 16:43:58 -0500

    Add benchmarks to compare with bracoxide

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,25 @@
+use brace_expander::BraceExpander;
+use bracoxide::{self, bracoxidize};
+use criterion::{Criterion, criterion_group, criterion_main};
+use std::hint::black_box;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let multiexpand = black_box("{1..10}{1..10}{1..10}{1..10}{1..10}");
+    c.bench_function("Multiexpand", |b| {
+        b.iter(|| BraceExpander::new().expand(multiexpand).unwrap())
+    });
+    c.bench_function("Multiexpand(bracoxide cmp)", |b| {
+        b.iter(|| bracoxidize(multiexpand).unwrap())
+    });
+
+    let simple = black_box("{a,b}");
+    c.bench_function("Simple", |b| {
+        b.iter(|| BraceExpander::new().expand(simple).unwrap())
+    });
+    c.bench_function("Simple(bracoxide cmp)", |b| {
+        b.iter(|| bracoxidize(simple).unwrap())
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
