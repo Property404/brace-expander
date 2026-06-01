3559bc286ac7876ef8f47331722e07f4a15ef33c
Property404 <git@dagans.dev>
2026-05-31 19:15:06 -0500

    Add timer option to example1

--- examples/example1.rs
+++ examples/example1.rs
@@ -1,12 +1,40 @@
 //! This program simply expands its arguments
 use brace_expander::BraceExpander;
+use std::{process::ExitCode, time::Instant};
 
-fn main() {
-    let args = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
+fn main() -> ExitCode {
+    // Parse arguments
+    let mut timer = false;
+    let mut args = std::env::args().skip(1).peekable();
+    while let Some(option) = args.next_if(|arg| arg.starts_with("-")) {
+        if option == "--timer" {
+            timer = true;
+        } else if option == "--" {
+            break;
+        } else {
+            eprintln!("Unknown option {option}");
+            return ExitCode::FAILURE;
+        }
+    }
+    let args = args.collect::<Vec<_>>().join(" ");
+
+    // Start recording time
+    let start = Instant::now();
+
+    // Expand
     let brace_expander = BraceExpander::default().ignore_parse_failures(true);
+    let expansion = brace_expander.expand(&args).expect("Infallible");
+    let expansion_time = Instant::now() - start;
 
-    println!(
-        "{}",
-        brace_expander.expand(&args).expect("Infallible").join(" ")
-    );
+    // Print results
+    println!("{}", expansion.join(" "));
+    let total_time = Instant::now() - start;
+
+    // Show elapsed time
+    if timer {
+        eprintln!("Expansion time: {expansion_time:?}");
+        eprintln!("total time    : {total_time:?}");
+    }
+
+    ExitCode::SUCCESS
 }
