04ea65d4742aeb99f9c3105221db469355eebaed
Property404 <git@dagans.dev>
2026-05-31 20:07:35 -0500

    New benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -5,27 +5,44 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let multiexpand = black_box("{1..10}{1..10}{1..10}{1..10}{1..10}");
-    c.bench_function("Multiexpand", |b| {
-        b.iter(|| BraceExpander::new().expand(multiexpand).unwrap())
-    });
-    c.bench_function("Multiexpand(old version)", |b| {
-        b.iter(|| OldBraceExpander::new().expand(multiexpand).unwrap())
-    });
-    c.bench_function("Multiexpand(bracoxide cmp)", |b| {
-        b.iter(|| bracoxidize(multiexpand).unwrap())
-    });
+    {
+        let multiexpand = black_box("{1..10}{1..10}{1..10}{1..10}{1..10}");
+        c.bench_function("Multiexpand", |b| {
+            b.iter(|| BraceExpander::new().expand(multiexpand).unwrap())
+        });
+        c.bench_function("Multiexpand(old version)", |b| {
+            b.iter(|| OldBraceExpander::new().expand(multiexpand).unwrap())
+        });
+        c.bench_function("Multiexpand(bracoxide cmp)", |b| {
+            b.iter(|| bracoxidize(multiexpand).unwrap())
+        });
+    }
 
-    let simple = black_box("{a,b}");
-    c.bench_function("Simple", |b| {
-        b.iter(|| BraceExpander::new().expand(simple).unwrap())
-    });
-    c.bench_function("Simple(old version)", |b| {
-        b.iter(|| OldBraceExpander::new().expand(simple).unwrap())
-    });
-    c.bench_function("Simple(bracoxide cmp)", |b| {
-        b.iter(|| bracoxidize(simple).unwrap())
-    });
+    {
+        let large_numeric = black_box("{1..300000}");
+        c.bench_function("Large Numeric", |b| {
+            b.iter(|| BraceExpander::new().expand(large_numeric).unwrap())
+        });
+        c.bench_function("Large Numeric(old version)", |b| {
+            b.iter(|| OldBraceExpander::new().expand(large_numeric).unwrap())
+        });
+        c.bench_function("Large Numeric(bracoxide cmp)", |b| {
+            b.iter(|| bracoxidize(large_numeric).unwrap())
+        });
+    }
+
+    {
+        let simple = black_box("{a,b}");
+        c.bench_function("Simple", |b| {
+            b.iter(|| BraceExpander::new().expand(simple).unwrap())
+        });
+        c.bench_function("Simple(old version)", |b| {
+            b.iter(|| OldBraceExpander::new().expand(simple).unwrap())
+        });
+        c.bench_function("Simple(bracoxide cmp)", |b| {
+            b.iter(|| bracoxidize(simple).unwrap())
+        });
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
